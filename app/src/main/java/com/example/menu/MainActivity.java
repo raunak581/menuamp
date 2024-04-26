0e8c6ea52bae28039bab4e6b3ac344cc64d072b0
raunak581 <raunakdubey681@gmail.com>
2024-04-26 09:32:46 +0530

    mdrgcedyj

---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.menu;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.i1){
+            Toast.makeText(this,"label 1 selected",Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.i2) {
+            Toast.makeText(this,"label 2 selected",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
